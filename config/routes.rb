ddbe3b77208d7c0bb14133a1041e9fbcc031bbd7
Arnspiger <carnspi@wireless-10-145-141-93.public.utexas.edu>
2018-07-12 14:38:23 -0500

    added update articles function and new controller books

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :articles
+  resources :books
 
   root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710213402) do
+ActiveRecord::Schema.define(version: 20180712160407) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
